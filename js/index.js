e4fb0cf8683dc5d0a750b67bf72eca026dea49dd
Omar Barbosa <omarbarbosa06@gmail.com>
2022-05-25 00:41:10 -0500

    Adding styles and generating cards using js

--- js/index.js
+++ js/index.js
@@ -8,3 +8,46 @@ const livesCounter = document.querySelector('span')
 const lives = 6
 
 livesCounter.textContent = lives
+
+//Random data order
+const randomizeData = () => {
+  return data.sort(() => Math.random() - 0.5)
+}
+
+//Generate html cards
+const cardGenerator = () => {
+  const randomizeCardData = randomizeData()
+  //Generate HTML
+  randomizeCardData.forEach((item) => {
+    const card = document.createElement('div')
+    const frontCard = document.createElement('img')
+    const backCard = document.createElement('div')
+
+    //Adding classes to each card
+    card.classList = 'card'
+    frontCard.classList = 'front'
+    backCard.classList = 'back'
+
+    //Attach info to card
+    frontCard.src = item.imgSrc
+
+    //Append cards to card area
+    cardArea.appendChild(card)
+    card.appendChild(frontCard)
+    card.appendChild(backCard)
+
+    addCardEventListener(card)
+  })
+}
+
+const addCardEventListener = (card) => {
+  card.addEventListener('click', (e) => {
+    card.classList.toggle('toggleCard')
+  })
+}
+
+//Check Cards
+const checkCards = (e) => {
+  const clickedCard = e.target
+}
+cardGenerator()
