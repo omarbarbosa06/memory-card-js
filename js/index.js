0b2629074a1f61e2f51209a577f1c2feb75f4927
Omar Barbosa <omarbarbosa06@gmail.com>
2022-05-31 01:03:48 -0500

    game logic

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ import data from '../data.json' assert { type: 'json' }
 const cardArea = document.querySelector('.cardArea')
 const livesCounter = document.querySelector('span')
 
-const lives = 6
+let lives = 3
 
 livesCounter.textContent = lives
 
@@ -13,15 +13,18 @@ livesCounter.textContent = lives
 const randomizeData = () => {
   return data.sort(() => Math.random() - 0.5)
 }
-
+const loseLife = () => {
+  lives--
+  livesCounter.textContent = lives
+}
 //Generate html cards
 const cardGenerator = () => {
   const randomizeCardData = randomizeData()
   //Generate HTML
   randomizeCardData.forEach((item) => {
     const card = document.createElement('div')
-    const frontCard = document.createElement('img')
-    const backCard = document.createElement('div')
+    const backCard = document.createElement('img')
+    const frontCard = document.createElement('div')
 
     //Adding classes to each card
     card.classList = 'card'
@@ -29,7 +32,8 @@ const cardGenerator = () => {
     backCard.classList = 'back'
 
     //Attach info to card
-    frontCard.src = item.imgSrc
+    backCard.src = item.imgSrc
+    card.setAttribute('name', item.name)
 
     //Append cards to card area
     cardArea.appendChild(card)
@@ -43,11 +47,55 @@ const cardGenerator = () => {
 const addCardEventListener = (card) => {
   card.addEventListener('click', (e) => {
     card.classList.toggle('toggleCard')
+    checkCards(e)
   })
 }
-
 //Check Cards
 const checkCards = (e) => {
   const clickedCard = e.target
+  clickedCard.classList.add('flipped')
+  const flippedCards = document.querySelectorAll('.flipped')
+  if (flippedCards.length === 2) {
+    if (
+      flippedCards[0].getAttribute('name') ===
+      flippedCards[1].getAttribute('name')
+    ) {
+      console.log('match')
+      flippedCards.forEach((card) => {
+        card.classList.remove('flipped')
+        card.style.pointerEvents = 'none'
+      })
+    } else {
+      console.log('not mtch')
+      flippedCards.forEach((card) => {
+        card.classList.remove('flipped')
+        setTimeout(() => {
+          card.classList.remove('toggleCard')
+        }, 1000)
+      })
+      loseLife()
+      if (lives === 0) {
+        restart()
+      }
+    }
+  }
+}
+//restart
+const restart = () => {
+  let data = randomizeData()
+  let back = document.querySelectorAll('.back')
+  let cards = document.querySelectorAll('.card')
+  cardArea.style.pointerEvents = 'none'
+  data.forEach((item, index) => {
+    cards[index].classList.remove('toggleCard')
+    setTimeout(() => {
+      cards[index].style.pointerEvents = 'all'
+      back[index].src = item.imgSrc
+      cards[index].setAttribute('name', item.name)
+      cardArea.style.pointerEvents = 'all'
+    }, 1000)
+  })
+  lives = 3
+  livesCounter.textContent = lives
 }
 cardGenerator()
